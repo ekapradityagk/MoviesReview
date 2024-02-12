3d82512a7a826ee7f669e1ca74afa30db2f626d5
ekapradityagk <ekapradityagk@gmail.com>
2024-02-12 14:43:27 +0700

    Initial Commit

---
+++ MoviesReview/MoviesReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesReviewApp.swift
+//  MoviesReview
+//
+//  Created by Eka Praditya on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
