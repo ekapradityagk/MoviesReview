6a8662df1cfb51b59fe67f54347663d634e181b1
ekapradityagk <ekapradityagk@gmail.com>
2024-02-15 05:56:32 +0700

    adding genre, movie list by genre and details, also review for the movie

---
+++ MovieDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailsView.swift
+//  MoviesReview
+//
+//  Created by Eka Praditya on 13/02/24.
+//
+
+import SwiftUI
+
+struct MovieDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailsView()
+}
---
+++ MovieReviewListView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieReviewListView.swift
+//  MoviesReview
+//
+//  Created by Eka Praditya on 14/02/24.
+//
+
+import SwiftUI
+
+struct MovieReviewListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieReviewListView()
+}
