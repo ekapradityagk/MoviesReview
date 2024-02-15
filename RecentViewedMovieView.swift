ff52efadb05e3c338750b6b98b72cdb5f813ef43
ekapradityagk <ekapradityagk@gmail.com>
2024-02-15 22:46:07 +0700

    adding youtube trailer

---
+++ RecentViewedMovieView.swift
@@ -0,0 +1,18 @@
+//
+//  RecentViewedMovieView.swift
+//  MoviesReview
+//
+//  Created by Eka Praditya on 15/02/24.
+//
+
+import SwiftUI
+
+struct RecentViewedMovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentViewedMovieView()
+}
---
+++ YouTubeVideoView.swift
@@ -0,0 +1,18 @@
+//
+//  YouTubeVideoView.swift
+//  MoviesReview
+//
+//  Created by Eka Praditya on 15/02/24.
+//
+
+import SwiftUI
+
+struct YouTubeVideoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTubeVideoView()
+}
